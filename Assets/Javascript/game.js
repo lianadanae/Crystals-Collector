f1ef614a92291a033880cfc3c90d73a4fc86ce38
lianadanae <lianabette@gmail.com>
2019-09-03 23:39:25 -0400

    updated files

---
+++ Assets/Javascript/game.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+
+// // The player will be shown a random number at the start of the game. 
+// Should be between 19 - 120
+var randomNbr = Math.floor(Math.random() * 120) + 19; 
+$('#randomnumber').html(randomNbr);
+
+})
+
+// // Each crystal should have a random hidden value between 1 - 12.
+var blue = Math.floor(Math.random() * 12) + 1;
+
+// // When the player clicks on a crystal, it will add a specific amount 
+// of points to the player's total score.
+var wins = 0;
+var losses = 0;
+
+var score = 0;
+$('#totalscore').text(score);
+
+function win() {
+    wins = wins + 1;
+    $('#wins').text(wins);
+    
+
+
+}
+// // Your game will hide this amount until the player clicks a crystal.
+// // When they do click one, update the player's score counter.
+
+
+
+// // The player wins if their total score matches the random number 
+// from the beginning of the game.
+
+
+// // The player loses if their score goes above the random number.
+
+
+// // The game restarts whenever the player wins or loses.
+
+// // When the game begins again, the player should see a new random number.
+//  Also, all the crystals will have four new hidden values. Of course, 
+//  the user's score (and score counter) will reset to zero.
+
+
+
+// // The app should show the number of games the player wins and loses. 
+// To that end, do not refresh the page as a means to restart the game.
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Unit-4-game
+
+The Crystals Collector Game
+
+How to Play:
+You will be given a random number at the start of the game. Use the crystals to match your points to the random number. The value of the crystals changes for every game and you must figure out their values on your own.
+
+Match your points to the random number to win. If you go over, you lose!
+
+Created with HTML, CSS, Javascript and Jquery
